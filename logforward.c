362c806e75eaefce48261b2f400641f4e725728c
Daniel Barlow <dan@telent.net>
2019-06-06 23:47:38 +0100

    write message to UDP socket
    - also, switch to newer syslog rfc

--- logforward.c
+++ logforward.c
@@ -1,17 +1,14 @@
 #include <fcntl.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-#include <arpa/inet.h>
 #include <sys/socket.h>
-
+#include <sys/stat.h>
 #include <sys/sysinfo.h>
-
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 /* 
 
@@ -47,7 +44,19 @@ void free_entry(struct entry *e) {
   free((void *)e);
 }
 
-#define MESSAGE_MAX (1024)	/* does my buf look big in this? */
+/* the minimum maximum MUST be at least 480 octets in length.
+
+   Any transport receiver MUST be able to accept messages of up to and
+   including 480 octets in length.  All transport receiver
+   implementations SHOULD be able to accept messages of up to and
+   including 2048 octets in length.  Transport receivers MAY receive
+   messages larger than 2048 octets in length. */
+
+#define MESSAGE_MAX (2048)	/* does my buf look big in this? */
+
+#ifndef HOST_NAME_MAX
+#define HOST_NAME_MAX 256
+#endif
 
 size_t count_newlines(char * buf)
 {
@@ -120,33 +129,96 @@ struct entry * read_entry(int fd)
 
 char *hostname="wombat";
 
-char *to_rfc_format(char *out_buf, size_t out_buf_len, struct entry *e)
+int to_rfc_format(char *out_buf, size_t out_buf_len, struct entry *e)
 {
-  /* rfc 3164 is ill-specified, but rfc 5424 is unused.  We go with the older
-   * version, except that we use iso timestamp */
+  /* rfc 5424 is the 'standard' for syslog.  73.6% of syslog
+   * sources "in the wild" don't conform to it, but the RFC was
+   * written by the author of rsyslog, so there is at least one
+   * halfway decent program available that will read it */
   char timestamp[40];
   struct tm tm;
   struct timespec ts;
   clock_gettime(CLOCK_REALTIME, &ts);
   gmtime_r(&(ts.tv_sec), &tm);
   strftime(timestamp, 40, "%FT%T", &tm);
-  snprintf(out_buf, out_buf_len, "<%ld> %s.%ldZ %s %s %s",
-	   e->prefix,
-	   timestamp,
-	   ts.tv_nsec,
-	   hostname,
-	   "kernel",
-	   e->message);
-  return out_buf;
+  return
+    snprintf(out_buf, out_buf_len, "<%ld>1 %s.%6ldZ %s %s %s %s %s %s",
+	     e->prefix,
+	     timestamp,
+	     (ts.tv_nsec / 1000),
+	     hostname,
+	     "kernel",		/* APP-NAME */
+	     "-",		/* no PROCID */
+	     "-",		/* no STRUCTURED-DATA */
+	     "-",		/* no MSGID */
+	     e->message);
 }
+
+int open_socket(const char *hostname, char* port)
+{
+   struct addrinfo hints;
+   struct addrinfo *result, *rp;
+   int sfd, s, j;
+   size_t len;
+   ssize_t nread;
+   
+   /* Obtain address(es) matching host/port */
+   
+   memset(&hints, 0, sizeof(struct addrinfo));
+   hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+   hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+   hints.ai_flags = 0;
+   hints.ai_protocol = 0;          /* Any protocol */
+   
+   s = getaddrinfo(hostname, port, &hints, &result);
+   if (s != 0) {
+     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+     exit(EXIT_FAILURE);
+   }
+   
+   /* getaddrinfo() returns a list of address structures.
+      Try each address until we successfully connect(2).
+      If socket(2) (or connect(2)) fails, we (close the socket
+      and) try the next address. */
+   
+   for (rp = result; rp != NULL; rp = rp->ai_next) {
+     sfd = socket(rp->ai_family, rp->ai_socktype,
+		  rp->ai_protocol);
+     if (sfd == -1)
+       continue;
+     printf("try %p ", rp->ai_addr);
+     if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
+       printf("works!\n");
+       break;                  /* Success */
+     }
+     close(sfd);
+   }
+   
+   if (rp == NULL) {               /* No address succeeded */
+     fprintf(stderr, "Could not connect\n");
+     exit(EXIT_FAILURE);
+   }
+   
+   freeaddrinfo(result);           /* No longer needed */
+
+   return sfd;
+}
+#include <errno.h>
+
 
 int main(int argc, char *argv[])
 {
+  /* $0  filename hostname port */
   int in_fd = open(argv[1], O_RDONLY);
-  char out_buf[1024];
+  char * port = (argc >= 4) ? argv[3] : "514";
+  int out_fd = open_socket(argv[2], port);
+  char out_buf[MESSAGE_MAX];
   struct entry *e;
   while(e = read_entry(in_fd)) {
-    puts(to_rfc_format(out_buf, 1024, e));
+    int bytes = to_rfc_format(out_buf, MESSAGE_MAX, e);
+    int written = write(out_fd, out_buf, bytes);
+    if(written < 0)
+      warn("could not send: ", strerror(errno));
   }    
 }
   
