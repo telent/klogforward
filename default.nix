e2462e0b87c66a7a83b0637f8ebcada185bfec83
Daniel Barlow <dan@telent.net>
2019-06-08 23:48:00 +0100

    working installPhase in derivation

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
-{stdenv, ...} : stdenv.mkDerivation {
+{stdenv ? (import <nixpkgs> {}).stdenv, ...} : stdenv.mkDerivation {
   name = "logforward";
   version = "1";
   src = ./.;
-  
+  installPhase = ''
+    mkdir -p $out/bin
+    make install DESTDIR=$out
+  '';
 }
